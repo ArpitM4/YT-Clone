5b3893acbea537bc841e06805c932e6627413c2e
arpit <arpitmaurya1506@gmail.com>
2024-12-12 13:51:51 +0530

    Your commit message

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 console.log("arpit")
+
 for (let i = 0; i < 20; i++) {
+    if (i==3){
+        setTimeout(() => {
+            console.log("waiting")
+        }, 3000);
+    }
 var newDiv = document.createElement("div");  // Create a new <div> element
 newDiv.className = "card";  // Add the class 'card' to the new <div>
 newDiv.innerHTML=`<div class="card">
-                <img src="https://i.ytimg.com/vi/n7Pwpxj3eQg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCNoBT7muXOL4LdvmAbeY7CXDMy3w"
+                <img src="https://picsum.photos/${400+i}/${225+i}"
                     alt="">
                 <div class="dgrid">
-                    <img src="https://yt3.ggpht.com/p89RToB7j2iu9BYhK2Zn6vLkxJXYC9cAbxfJ0eMvUd4jrsyRQtuYIvinn8N2H6utyeBKcrvlhSE=s68-c-k-c0x00ffffff-no-rj"
+                    <img src="https://picsum.photos/${100+i}"
                         alt="">
                     <div class="spa">
-                        <span class="title">Video Title</span>
+                        <span class="title">Video Title ${i+1}</span>
                         <span class="cname">Channel Name</span>
-                        <span class="views">135K ~ 11h hours ago</span>
+                        <span class="views">${Math.floor(100*(Math.random()))}K • ${Math.floor(24*(Math.random()))}h hours ago</span>
                     </div>
                     <span class="op">:</span>
                 </div>
